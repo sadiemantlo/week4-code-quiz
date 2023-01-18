8e2910a1993a59087dfb6022c852be4ea9ae2fab
Sadie Mantlo <sadiemantlo@Sadies-MacBook-Air.local>
2023-01-17 17:35:19 -0800

    added a timer function

--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,118 @@
 // Global variables
 const startBttn = document.getElementById('start-bttn')
-const questionEl =document.getElementById('question-container')
+const questionContainerEl = document.getElementById('question-container')
+const questionEl = document.getElementById('questions')
+const answerBttnEl = document.getElementById('answer-bttns')
+var questionIndex = 0
+var timerEl = document.getElementById('timer')
 // list of all questions, choices, and answers
 const questions = [
     {
-      title: 'Commonly used data types DO NOT include:',
-      choices: [
+      question: 'Commonly used data types DO NOT include:',
+      answer: [
         {text: 'strings', correct: false},
         {text: 'booleans', correct: false},
         {text: 'alerts', correct: true},
         {text: 'numbers', correct: false},
-      ],
+      ]
     },
     {
-      title: 'The condition in an if / else statement is enclosed within ____.',
-      choices: [
+      question: 'The condition in an if / else statement is enclosed within ____.',
+      answer: [
         {text: 'quotes', correct: false},
         {text: 'curly brackets', correct: false},
         {text: 'parentheses', correct: true},
         {text: 'square brackets', correct: false}
-      ],
+      ]
     },
     {
-      title: 'Arrays in JavaScript can be used to store ____.',
-      choices: [
+      question: 'Arrays in JavaScript can be used to store ____.',
+      answer: [
         {text: 'numbers and strings', correct: false},
         {text: 'other arrays', correct: false},
         {text: 'booleans', correct: false},
         {text: 'all of the above', correct: true},
-      ],
+      ]
     },
     {
-      title:
+      question:
         'String values must be enclosed within ____ when being assigned to variables.',
-      choices: [
+      answer: [
         {text: 'commas', correct: false},
         {text: 'curly brackets', correct: false},
         {text: 'quotes', correct: true},
         {text: 'parentheses', correct: false},
-      ],
+      ]
     },
     {
-      title:
+      question:
         'A very useful tool used during development and debugging for printing content to the debugger is:',
-      choices: [
+      answer: [
         {text: 'JavaScript', correct: false},
         {text: 'terminal / bash', correct: false},
         {text: 'for loops', correct: false},
         {text: 'console.log', correct: true},
-      ],
+      ]
     },
   ];
 
 // start game
 startBttn.addEventListener('click', startGame)
 function startGame() {
-  console.log("eheheh");
+  // startTimer();
   startBttn.style.display = "none";
-  nextQuestion()
+  nextQuestion();
+}
+
+// start timer 
+startBttn.addEventListener('click', startTimer)
+var timeLeft = 76;
+function startTimer() {
+  var timer = setInterval(function() {
+    timeLeft--;
+    timerEl.textContent = timeLeft + ' seconds left';
+    if(timeLeft === 0) {
+      clearInterval(timer);
+      sendMessage();
+    }
+  }, 1000);
 }
 
 // setting the next question
+var question = questions[0]
 function nextQuestion() {
+  resetState()
+  displayQuestion(questions.questionIndex)
+    for (i = 0; i < questions.length; i++) {
 
+    }
+}
+
+var displayQuestion = function() {
+  questionEl.innerText = question.question
+  question.answer.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('bttn')
+    button.addEventListener('click', selectAnswer)
+    answerBttnEl.appendChild(button)
+  })
+}
+
+function resetState() {
+  while (answerBttnEl.firstChild) {
+    answerBttnEl.removeChild
+    (answerBttnEl.firstChild)
+  }
+}
+
+// answer buttons 
+function selectAnswer(e) {
+  const selectedBttn = e.target 
+  const correct = selectedBttn.dataset.correct
+  Array.from(answerBttnEl.children).forEach(button => {
+    
+  })
 }
 
 
@@ -71,19 +121,16 @@ function nextQuestion() {
 
 
 
-// var timeLeft = 76;
+
+
+
+
+
+
+var timeLeft = 76;
 // function newQuiz() {
 //   questionIndex = 0;
   
 // }
-// function startTimer() {
-//   var timer = setInterval(function() {
-//     timeLeft--;
-//     timerElement.textContent = timeLeft + ' seconds left';
-//     if(timeLeft === 0) {
-//       clearInterval(timer);
-//       sendMessage();
-//     }
-//   }, 1000);
-// }
+
 // startBttn.addEventListener("click", startTimer);
